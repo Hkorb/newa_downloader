04870ab19a7fdc641a9f7ed35f88279e74ae8fab
Hkorb <henry.korb@web.de>
2023-04-15 16:27:35 +0200

    remove build files and add to gitignore

--- build/lib/newa_downloader/__init__.py
+++
@@ -1,3 +0,0 @@
-from .mesoscale.climate import download as download_mesoscale_climate
-from .mesoscale.timeseries import download as download_mesoscale_timeseries
-from .microscale.climate import download as download_microscale_climate
